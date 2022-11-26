b1e10cefaeedfd0bedbf88a66b95774fb36b67a0
Kaustubh Singh <96336281+kaustubhsingh47@users.noreply.github.com>
2022-11-26 15:50:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.20.0"
+    }
+  }
+}
+
 provider "google" {
+  credentials = file("./SA.json")
   project = "linear-quasar-368909"
   region  = "us-central1"
 }
-
