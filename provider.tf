ee0283be2220706f839e50887dee963c646c935b
Kaustubh Singh <96336281+kaustubhsingh47@users.noreply.github.com>
2022-11-26 15:46:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,10 +3,4 @@ provider "google" {
   project = "linear-quasar-368909"
   region  = "us-central1"
 }
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-  }
 
